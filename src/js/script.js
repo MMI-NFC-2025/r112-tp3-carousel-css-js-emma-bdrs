68120b3aeec68f06bcb6d76f03f9970e1f232c41
emma-bdrs <emma.badarous@edu.univ-fcomte.fr>
2025-11-19 14:55:03 +0100

    changement

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,17 @@
 // *** CAROUSEL ***
+document.addEventListener( 'DOMContentLoaded', function() {
+  const carouselContainer = document.querySelector('.carousel__container');
+  const prevButton = document.querySelector('.carousel__button--prev');
+  const nextButton = document.querySelector('.carousel__button--next');
+  const itemWidth = carouselContainer.querySelector('.carousel__item').offsetWidth + 16; 
+  const premierItem = carouselContainer.querySelector('.carousel__item');
+
+  prevButton.addEventListener('click', function() {
+    carouselContainer.scrollBy({
+      left: -itemWidth,
+      behavior: 'smooth'
+    });
+  });
+}
+
 // TODO
